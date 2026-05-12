8c72a0385e21dc83a0253089050066abb4eb85d4
Akshay Gund <akshaygund07@outlook.com>
2026-05-12 19:37:44 +0530

    Refactor user registration and login handling, enhance exception management, and update Docker configuration for timezone support

--- StoreMaster/src/main/java/com/akshay/StoreMaster/StoreMasterApplication.java
+++ StoreMaster/src/main/java/com/akshay/StoreMaster/StoreMasterApplication.java
@@ -6,11 +6,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EntityScan(basePackages = "com.akshay.StoreMaster.entity")
 public class StoreMasterApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/kolkata"));
 		SpringApplication.run(StoreMasterApplication.class, args);
 	}
 	@Bean
